17f3d7123022525c3f6b11174b8fa78f5fa7cd2d
EC2 Default User <ec2-user@ip-172-31-7-244.us-west-2.compute.internal>
2016-12-07 20:59:22 +0000

    initialSetup.sh

--- initialSetup.sh
+++ initialSetup.sh
@@ -1,26 +1,20 @@
 #!/bin/bash -e
 
-export VIRTUAL_ENV="/home/ec2-user/DataVisualization/website/venv"
-export PATH="$VIRTUAL_ENV/bin:$PATH"
-unset PYTHON_HOME
+#export VIRTUAL_ENV="/home/ec2-user/DataVisualization/website/venv"
+#export PATH="$VIRTUAL_ENV/bin:$PATH"
+#unset PYTHON_HOME
 #exec "${@:-$SHELL}"
 
 sudo yum -y install mysql mysql-server gcc gcc-c++ mysql-devel
 #git clone https://github.com/rmaheshkumarblr/DataVisualization.git
 virtualenv -p /usr/bin/python2.7 /home/ec2-user/DataVisualization/website/venv
-pip install -r /home/ec2-user/DataVisualization/requirements.txt
 
-#echo "bind-address = localhost" >> /etc/my.cnf
+/home/ec2-user/DataVisualization/website/venv/bin/pip install cython
+/home/ec2-user/DataVisualization/website/venv/bin/pip install -r /home/ec2-user/DataVisualization/requirements.txt
 
 sudo /etc/init.d/mysqld restart
 
-mysql << EOF
-UPDATE user SET Password=PASSWORD('data') WHERE User='root';
-flush privileges;
-exit;
-EOF
-
-mysql -u root -pdata << EOF
+mysql -uroot << EOF
 create database datavisdb;
 create user datavis;
 grant all on datavisdb.* to 'datavis'@'localhost' identified by 'data';
