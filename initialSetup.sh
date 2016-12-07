95eb477e3973a0b7f9c92814c460e0256dfdcb66
EC2 Default User <ec2-user@ip-172-31-8-204.us-west-2.compute.internal>
2016-12-07 20:21:06 +0000

    initialSetup.sh

--- initialSetup.sh
+++ initialSetup.sh
@@ -5,14 +5,14 @@ export PATH="$VIRTUAL_ENV/bin:$PATH"
 unset PYTHON_HOME
 #exec "${@:-$SHELL}"
 
-yum -y install git mysql mysql-server gcc gcc-c++ mysql-devel
-git clone https://github.com/rmaheshkumarblr/DataVisualization.git
-virtualenv -p /usr/bin/python2.7 DataVisualization/website/venv
-pip install -r DataVisualization/requirements.txt
+sudo yum -y install mysql mysql-server gcc gcc-c++ mysql-devel
+#git clone https://github.com/rmaheshkumarblr/DataVisualization.git
+virtualenv -p /usr/bin/python2.7 /home/ec2-user/DataVisualization/website/venv
+pip install -r /home/ec2-user/DataVisualization/requirements.txt
 
 #echo "bind-address = localhost" >> /etc/my.cnf
 
-/etc/init.d/mysqld restart
+sudo /etc/init.d/mysqld restart
 
 mysql << EOF
 UPDATE user SET Password=PASSWORD('data') WHERE User='root';
