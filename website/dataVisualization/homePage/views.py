51c3fc10df8ffc10795123654a9f747618abe71f
Vaidehi <Vaidehi Salway>
2021-01-15 02:21:11 -0700

    old code

--- website/dataVisualization/homePage/views.py
+++ website/dataVisualization/homePage/views.py
@@ -7,8 +7,8 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse,JsonResponse
-from . import Document
-from . import DocumentForm, LoginForm, CreateUserForm
+from models import Document
+from forms import DocumentForm, LoginForm, CreateUserForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
